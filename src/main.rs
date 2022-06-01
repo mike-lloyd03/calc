5d1ef4394794f509699c88a4e27eee26f1a59a80
Mike Lloyd <mike.lloyd@teradata.com>
2022-06-01 15:08:19 -0700

    Implement conversions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use eval::eval;
 use regex::Regex;
-use std::env::args;
+use rink_core::{one_line, simple_context};
 use std::process::exit;
 
 /// Run basic calculations or unit conversions
@@ -18,22 +18,29 @@ struct Cli {
 
 fn main() {
     let cli = Cli::parse();
-    let input: String = cli.input.join(" ");
+    let input: &str = &cli.input.join(" ");
+    if input.is_empty() {
+        eprintln!("No input provided");
+        exit(1);
+    }
 
-    let result = eval_equation(&input);
+    let result = match cli.convert {
+        true => eval_conversion(input),
+        false => eval_equation(input),
+    };
     println!("{}", result);
 }
 
 fn eval_equation(equation: &str) -> String {
     if !validate_equation(equation) {
-        println!("Invalid characters in input");
+        eprintln!("Invalid characters in input");
         exit(1);
     }
 
     match eval(equation) {
         Ok(result) => result.to_string(),
         Err(_) => {
-            println!("Unable to parse input");
+            eprintln!("Unable to parse input");
             exit(1);
         }
     }
@@ -42,6 +49,34 @@ fn eval_equation(equation: &str) -> String {
 fn validate_equation(input: &str) -> bool {
     let re = Regex::new(r"[\d\+\-\*/\s\(\)]").expect("invalid regex");
     input.chars().all(|c| re.is_match(&c.to_string()))
+}
+
+fn eval_conversion(input: &str) -> String {
+    let re = Regex::new(
+        r"(?P<val>-?\d+\.?\d*)\s?(?P<from_unit>[a-zA-Z/°]+)\s(->?|to)\s(?P<to_unit>[a-zA-Z/°]+)",
+    )
+    .expect("invalid regex");
+    let caps = match re.captures(input) {
+        Some(c) => c,
+        None => {
+            eprintln!("Unable to parse input. Conversion strings should be in the form: '<Value> <Unit> -> <Unit>' (e.g. '12 ft -> m')");
+            exit(1);
+        }
+    };
+    let value = caps.name("val").unwrap().as_str();
+    let from_unit = caps.name("from_unit").unwrap().as_str();
+    let to_unit = caps.name("to_unit").unwrap().as_str();
+    let expression = format!("{} {} -> {}", value, from_unit, to_unit);
+
+    // rink
+    let mut ctx = simple_context().unwrap();
+    match one_line(&mut ctx, &expression) {
+        Ok(r) => r.split('(').next().unwrap().trim().to_string(),
+        Err(_) => {
+            eprintln!("Units are invalid");
+            exit(1);
+        }
+    }
 }
 
 #[test]
@@ -63,4 +98,19 @@ fn test_eval_equation() {
     assert_eq!(eval_equation("1 / 2"), "0.5");
     assert_eq!(eval_equation("1 * 2"), "2");
     assert_eq!(eval_equation("1 - 2"), "-1");
+}
+
+#[test]
+fn test_eval_conversion() {
+    assert_eq!(eval_conversion("1 ft -> inch"), "12 inch");
+    assert_eq!(eval_conversion("1 mi -> ft"), "5280 foot");
+    assert_eq!(eval_conversion("10cm -> m"), "0.1 meter");
+    assert_eq!(eval_conversion("-40 degC -> degF"), "-40 °F");
+    assert_eq!(eval_conversion("100 kph -> mph"), "approx. 62.13711 mph");
+    assert_eq!(
+        eval_conversion("100 km/hr -> mi/hr"),
+        "approx. 62.13711 mile / hour"
+    );
+    assert_eq!(eval_conversion("1 ft - inch"), "12 inch");
+    assert_eq!(eval_conversion("1 ft to inch"), "12 inch");
 }
