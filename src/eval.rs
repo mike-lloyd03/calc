d3679a4d50c2d09eb418ba2c969e55bec4807424
Mike Lloyd <mike.lloyd03@pm.me>
2022-06-13 21:05:03 -0700

    Support 'x' for multiplication

--- src/eval.rs
+++ src/eval.rs
@@ -9,7 +9,7 @@ pub fn evaluate(equation: &str) -> String {
         exit(1);
     }
 
-    match eval(equation) {
+    match eval(&equation.to_lowercase().replace('x', "*")) {
         Ok(result) => result.to_string(),
         Err(_) => {
             eprintln!("Unable to parse input");
@@ -20,7 +20,7 @@ pub fn evaluate(equation: &str) -> String {
 
 /// Ensure the input equation doesn't have any invalid characters before it is passed to `eval`
 fn validate_equation(input: &str) -> bool {
-    let re = Regex::new(r"[\d\+\-\*/\s\(\)\.]").expect("invalid regex");
+    let re = Regex::new(r"[\d\+\-\*/\s\(\)\.xX]").expect("invalid regex");
     input.chars().all(|c| re.is_match(&c.to_string()))
 }
 
@@ -33,6 +33,8 @@ fn test_validate_equation() {
     assert!(validate_equation("1 * 2"));
     assert!(validate_equation("1 - 2"));
     assert!(validate_equation("1+2"));
+    assert!(validate_equation("5 x 2"));
+    assert!(validate_equation("5 X 2"));
 
     assert!(!validate_equation("1 + k"));
 }
@@ -45,4 +47,6 @@ fn test_eval_equation() {
     assert_eq!(evaluate("1 * 2"), "2");
     assert_eq!(evaluate("1 - 2"), "-1");
     assert_eq!(evaluate("10.2 / 2.5"), "4.08");
+    assert_eq!(evaluate("5 x 2"), "10");
+    assert_eq!(evaluate("5 X 2"), "10");
 }
